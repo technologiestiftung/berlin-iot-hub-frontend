4272a148837bd1939d741206be0fd1b4c377e7ce
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-08 13:26:15 +0200

    feat(header): add basic markup and styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, Flex, Box, Heading, Image, Text } from "theme-ui";
+
+const DatahubLogo = "/images/datahub-logo.svg";
+const TSBLogo = "/images/tsb-logo.svg";
+
+export const Header: React.FC<any> = () => {
+  return (
+    <Flex
+      p={4}
+      color="text"
+      bg="background"
+      sx={{ boxShadow: "0 2px 4px rgba(0,0,0,0.2)" }}
+    >
+      <Box sx={{ flex: "1 1 auto" }}>
+        <Flex>
+          <Image
+            src={DatahubLogo}
+            alt={"Logo des Berlin Data Hub"}
+            sx={{ width: "56px" }}
+          />
+          <Heading ml={3} sx={{ lineHeight: "inherit" }}>
+            Berlin <span sx={{ fontWeight: "normal" }}>Data Hub</span>
+          </Heading>
+        </Flex>
+      </Box>
+      <Box>
+        <Flex>
+          <Text>Ein Projekt der:</Text>
+          <Image
+            src={TSBLogo}
+            alt={"Logo der Technologiestiftung Berlin"}
+            ml={3}
+            sx={{ width: "148px" }}
+          />
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
