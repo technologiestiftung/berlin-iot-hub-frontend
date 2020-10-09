19640dd334aa72c9136a7b5aec3dded5d06abec6
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-09 17:17:21 +0200

    add interface to params

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,8 +2,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { jsx } from "theme-ui";
-
+interface RouteParams {
+  id: string;
+}
 export const Project: React.FC = () => {
-  let { id } = useParams<{id: string}>();
+  let { id } = useParams<RouteParams>();
   return <h1>Ein Projekt mit der ID: {id}</h1>;
 };
