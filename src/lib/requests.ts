64858e1dd6dae6ccad4481d45666ae964923c52d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-19 13:14:07 +0200

    feat(api): provide request for device records

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -1,11 +1,8 @@
+import { DeviceType } from "../state/model";
+
 export interface DeviceResponse {
   data: {
-    devices: {
-      id: number;
-      ttnDeviceId: string;
-      ttnAppId: string;
-      description: string;
-    };
+    devices: Array<DeviceType>;
   };
 }
 
@@ -16,5 +13,26 @@ export async function getDevices(url: string): Promise<DeviceResponse> {
     throw new Error("Failed to fetch device");
   }
   const json = (await response.json()) as DeviceResponse;
+  return json;
+}
+
+export interface RecordsResponse {
+  data: {
+    records: Array<{
+      id: number;
+      deviceId: number;
+      recordedAt: string;
+      value: number;
+    }>;
+  };
+}
+
+export async function getRecords(url: string): Promise<RecordsResponse> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.error(await response.text());
+    throw new Error("Failed to fetch device");
+  }
+  const json = (await response.json()) as RecordsResponse;
   return json;
 }
