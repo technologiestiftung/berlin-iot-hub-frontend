9dfab87631db83b720e3607a320dbf08d2cf5305
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-19 13:19:22 +0200

    chore: format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useStoreActions } from "./state/hooks";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Overview } from "./components/Overview";
 import { Project } from "./components/Project";
 
-function App() {
+const App: React.FC = () => {
   const loadDevices = useStoreActions((action) => action.projects.load);
 
   useEffect(() => {
@@ -13,18 +13,16 @@ function App() {
   }, [loadDevices]);
 
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Overview />
-          </Route>
-          <Route path="/:id" children={<Project />}></Route>
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Overview />
+        </Route>
+        <Route path="/:id" children={<Project />}></Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
