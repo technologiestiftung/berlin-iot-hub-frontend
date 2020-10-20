0d3e53ac2b9f3baa59975afccb9b50eaa3a92143
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-20 13:33:08 +0200

    Add interface and type central station related to DATAHUB-38 DATAHUB-37

---
+++ src/common/interface.ts
@@ -0,0 +1,4 @@
+// TODO: [DATAHUB-37] Migrate all interfaces into this central file
+export interface FooBah {}
+
+interface Foo extends FooBah {}
---
+++ src/common/types.ts
@@ -0,0 +1,2 @@
+// TODO: [DATAHUB-38] Migrate all types into this central file
+export type Baz = "Baz" | "Bah";
