857dc9eb287b387575e3cdeb4bfd1bc6a61d2866
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-22 17:09:44 +0200

    feat(DATAHUB-23): implement native CSV download

---
+++ src/lib/download-handlers.ts
@@ -0,0 +1,26 @@
+import { RecordType } from "../common/interfaces";
+
+export const createCSVStructure = (input: RecordType[] | undefined): string => {
+  let csv = "id,deviceId,recordedAt,value\n";
+
+  if (!input) return csv;
+
+  input.forEach((record: RecordType) => {
+    csv += `${record.id},${record.deviceId},${record.recordedAt},${record.value},`;
+    csv += "\n";
+  });
+
+  return csv;
+};
+
+export const downloadCSV = (input: string, title: string | undefined): void => {
+  let hiddenElement = document.createElement("a");
+  hiddenElement.href = `data:text/csv;charset=utf-8,${encodeURI(input)}`;
+  hiddenElement.target = "_blank";
+  hiddenElement.download = `${
+    // @ts-ignore replaceAll IS implemented in all modern browsers
+    title ? title.toLowerCase().replaceAll(" ", "_") : "data"
+  }.csv`;
+  hiddenElement.click();
+  hiddenElement.remove();
+};
