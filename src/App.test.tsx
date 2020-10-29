a768c4322ce6dc633c0295f1ed1d4971fb280c76
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-29 15:58:56 +0100

    test: provide theme to App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,11 +3,15 @@ import { render } from "@testing-library/react";
 import App from "./App";
 import { StoreProvider } from "easy-peasy";
 import store from "./state/store";
+import { ThemeProvider } from "theme-ui";
+import theme from "./style/theme";
 
 test("renders claim element", () => {
   const { getByText } = render(
     <StoreProvider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </StoreProvider>
   );
   const claimElement = getByText(/Offene Datenplattform für IoT-Projekte/i);
