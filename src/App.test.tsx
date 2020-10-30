9d203590e0f9be3aa4e478859a3409788e3d8388
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-30 17:36:01 +0100

    test(DATAHUB-45): test home page with mock data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitForElement } from "@testing-library/react";
 import App from "./App";
 import { StoreProvider } from "easy-peasy";
 import store from "./state/store";
 
-test("renders claim element", () => {
-  const { getByText } = render(
-    <StoreProvider store={store}>
-      <App />
-    </StoreProvider>
-  );
-  const claimElement = getByText(/Offene Datenplattform für IoT-Projekte/i);
-  expect(claimElement).toBeInTheDocument();
+describe("Home page", () => {
+  test("renders claim element", async () => {
+    render(
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
+    );
+    const claimElement = await screen.findByText(
+      /Offene Datenplattform für IoT-Projekte/i
+    );
+    expect(claimElement).toBeInTheDocument();
+  });
+  test("displays headings of projects", async () => {
+    render(
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
+    );
+
+    const projectAHeading = await waitForElement(() =>
+      screen.findByText("Test project A")
+    );
+    const projectBHeading = await waitForElement(() =>
+      screen.findByText("Test project B")
+    );
+    expect(projectAHeading).toBeInTheDocument();
+    expect(projectBHeading).toBeInTheDocument();
+  });
 });
