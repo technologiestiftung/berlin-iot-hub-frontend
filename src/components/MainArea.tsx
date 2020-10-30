6953bc25e04858f37b4fe477e7b830c1e3858640
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-10-30 15:11:32 +0100

    feat(DATAHUB-90): provide not-found route

--- src/components/MainArea.tsx
+++ src/components/MainArea.tsx
@@ -5,15 +5,17 @@ import { jsx } from "theme-ui";
 
 import { Overview } from "./Overview";
 import { Project } from "./Project";
+import { NotFoundPage } from "./NotFoundPage";
 
 export const MainArea: React.FC = () => {
   return (
-    <main sx={{ zIndex: 0 }}>
+    <main sx={{ minHeight: "100vh", zIndex: 0 }}>
       <Switch>
         <Route exact path="/">
           <Overview />
         </Route>
         <Route path="/:id" children={<Project />}></Route>
+        <Route component={NotFoundPage} />
       </Switch>
     </main>
   );
