10b39df11fd8e7ef4e88ef04ccd3bcfa1594e03e
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-11-04 16:44:18 +0100

    test(DATAHUB-45): test elements on home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitForElement } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { StoreProvider } from "easy-peasy";
 import store from "./state/store";
@@ -8,14 +8,47 @@ import theme from "./style/theme";
 
 window.scrollTo = jest.fn();
 
-test("renders claim element", () => {
-  const { getByText } = render(
-    <StoreProvider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </StoreProvider>
-  );
-  const claimElement = getByText(/Offene Datenplattform für IoT-Projekte/i);
-  expect(claimElement).toBeInTheDocument();
+describe("Home page / project overview", () => {
+  test("renders claim element", () => {
+    render(
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </StoreProvider>
+    );
+
+    const claimElement = screen.getByText(
+      /Offene Datenplattform für IoT-Projekte/i
+    );
+    expect(claimElement).toBeInTheDocument();
+  });
+
+  test("displays headings of projects", async () => {
+    render(
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </StoreProvider>
+    );
+
+    const projectAHeading = await screen.findByText(/Test project A/i);
+    const projectBHeading = await screen.findByText(/Test project B/i);
+    expect(projectAHeading).toBeInTheDocument();
+    expect(projectBHeading).toBeInTheDocument();
+  });
+
+  test("displays cookie disclaimer", () => {
+    render(
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </StoreProvider>
+    );
+
+    const cookieBanner = screen.getByText(/Diese Webseite verwendet Cookies/i);
+    expect(cookieBanner).toBeInTheDocument();
+  });
 });
