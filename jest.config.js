b857f04ec069a3983ccb8155dec64bbc37eedf75
Lucas Vogel <contact@vogelino.com>
2021-04-07 16:24:07 +0200

    ci(deps): Add dependencies and config files

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	moduleNameMapper: {
+		'^@components(.*)$': '<rootDir>/src/components$1',
+		'^@utils(.*)$': '<rootDir>/src/utils$1',
+		'^@state(.*)$': '<rootDir>/src/state$1',
+	},
+}
